e9d1d5e664256a09bd3c30f5db0cab9aacfa0bba
boshka0 <nastyaboksha@gmail.com>
2018-09-13 19:58:54 +0300

    Implement getLoveTrianglesCount function

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var first, second, third, count = 0;
+  for(var n = 0; n < preferences.length; n++){
+    first = preferences[n];
+    second = preferences[first-1];
+    third = preferences[second-1];
+    if(n+1 == third) count++;
+  }
+  return Math.floor(count/3);
 };
